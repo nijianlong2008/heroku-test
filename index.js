680198073527f8d2224590815f1cebdf45f309db
倪建龙 <nijianlong@ruifucredit.com>
2019-07-11 17:21:35 +0800

    修改

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World22!'))
+app.get('/', (req, res) => res.send('Hello World444422!'))
 
 const port = process.env.PORT || 3000
 
