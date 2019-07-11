1e607056a709e0100e2e6afed5075f6d3c732759
倪建龙 <nijianlong@ruifucredit.com>
2019-07-11 17:51:19 +0800

    提交

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World123!'))
+app.get('/', (req, res) => {
+    let str = '啦啦啦啦啦啦'
+    if(req.query.type == 1){
+        str = '雷总你大爷的'
+    }
+    return res.send(str)
+})
 
 var port = process.env.PORT || '3000'
 
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
